aa748528b15ddbe3d62d8750bd4e63326ea4defa
Genki_Arai <aragen0work@gmail.com>
2025-03-16 16:51:57 +0900

    p121まで

--- components/Section_Todo.js
+++ components/Section_Todo.js
@@ -1,57 +1,94 @@
-import { Button, FlatList, Platform, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
 
+// おそらく、StatusbarHeightがあるため、Web版で表示されない
 const StatusbarHeight = Platform.OS === "ios" ? 44 : 56;
 
 export default function Section_Todo() {
-    const [todo, setTodo] = React.useState({
-        todo:[
-        {index: 1, title: "todo1", done: false},
-        {index: 2, title: "todo2", done: false},
-        {index: 3, title: "todo3", done: false}],
-        currentIndex: 3,
-    });
-    const [inputText, setInputText] = React.useState("");
+  const [todo, setTodo] = React.useState({
+    todo: [
+      { index: 1, title: "todo1", done: false },
+      { index: 2, title: "todo2", done: false },
+      { index: 3, title: "todo3", done: false },
+    ],
+    currentIndex: 3,
+  });
+  const [inputText, setInputText] = React.useState("");
+  const [filterText, setFilterText] = React.useState("");
+  const [todoList, setTodoList] = React.useState(todo.todo);
 
-    const addTodo = () => {
-        if(!inputText){
-            return;
-        }
-        const newTodo = {
-            index: todo.currentIndex + 1,
-            title: inputText,
-            done: false,
-        };
-        setTodo({
-            todo: [...todo.todo, newTodo],
-            currentIndex: todo.currentIndex + 1,
-        })
-        setInputText("");
+  const addTodo = () => {
+    if (!inputText) {
+      return;
     }
+    const newTodo = {
+      index: todo.currentIndex + 1,
+      title: inputText,
+      done: false,
+    };
+    setTodo({
+      todo: [...todo.todo, newTodo],
+      currentIndex: todo.currentIndex + 1,
+    });
+    setTodoList([...todoList, newTodo]);
+    setInputText("");
+  };
 
+  const filterTodo = () => {
+    setTodoList(
+      todo.todo.filter((todo) => {
+        return todo.title.includes(filterText);
+      })
+    );
+  };
 
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
       <View style={styles.filter}>
-        <Text>open Section_Todo.js!!</Text>
+        <View style={styles.inputArea}>
+          <TextInput
+            style={styles.input}
+            onChangeText={setFilterText}
+            placeholder="Todoの検索"
+            value={filterText}
+          ></TextInput>
+          <Button title="検索" onPress={filterTodo}></Button>
+        </View>
       </View>
       <ScrollView style={styles.todolist}>
         <Text>ScrollView</Text>
-        <FlatList 
-            data={todo.todo} 
-            renderItem={({item}) => <Text>{item.title}</Text>}
-            keyExtractor={(item, index) => "todo_" + item.index}
-            ></FlatList>
+        <FlatList
+          data={todoList}
+          renderItem={({ item }) => (
+            <Text key={item.title + "_" + item.index}>{item.title}</Text>
+          )}
+          keyExtractor={(item, index) => "todo_" + item.index}
+        ></FlatList>
+        <TouchableOpacity>
+          <Text>タップしてください</Text>
+        </TouchableOpacity>
       </ScrollView>
-      <View style={styles.input}>
+      <View style={styles.inputArea}>
         <TextInput
-            placeholder="Todoを入力してください"
-            onChangeText={setInputText}
-            value={inputText}
+          placeholder="Todoを入力してください"
+          onChangeText={setInputText}
+          value={inputText}
+          style={styles.input}
         ></TextInput>
         <Button title="追加" onPress={addTodo}></Button>
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -62,12 +99,21 @@ const styles = StyleSheet.create({
     marginVertical: StatusbarHeight,
   },
   filter: {
-    height: 30,
+    height: 50,
   },
   todolist: {
     flex: 1,
   },
-  input : {
-    height: 30,
-  }
+  inputArea: {
+    height: 40,
+    flexDirection: "row",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#000",
+    flex: 1,
+  },
+  button: {
+    width: 30,
+  },
 });
